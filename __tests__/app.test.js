14f8e79df3ea7dae7a49f223d8b82c89d3212e04
Brian McPhillips <B.McPhillips89@gmail.com>
2020-09-22 16:33:50 -0700

    controller created & app.js updated

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -35,6 +35,22 @@ describe('recipe-lab routes', () => {
       });
   });
 
+  it('gets a recipe by id', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
+
+    const response = await request(app).get(`/api/v1/recipes/${recipe.id}`);
+
+    expect(response.body.toEqual(recipe))
+  });
+
   it('gets all recipes', async() => {
     const recipes = await Promise.all([
       { name: 'cookies', directions: [] },
@@ -85,5 +101,20 @@ describe('recipe-lab routes', () => {
           ]
         });
       });
+  });
+
+  it('deletes a recipe by id', async() => {
+    const recipe = await Recipe.insert({
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
+
+    const response = await request(app).delete(`/api/v1/recipes/${recipe.id}`);
+
+    expect(response.body).toEqual(recipe)
   });
 });
